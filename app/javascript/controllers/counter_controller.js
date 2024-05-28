49eaee3497350c55353baa0132a062162e111f1c
Yugie-oh <muthige.l@gmail.com>
2024-05-28 16:11:58 +0200

    second commit

---
+++ app/javascript/controllers/counter_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="counter"
+export default class extends Controller {
+  static targets = ["output"]
+  connect() {
+    this.count = 0
+    console.log('hello', this.element)
+    console.log('hi', this.outputTarget)
+  }
+  increment() {
+    this.count += 1
+    this.outputTarget.textContent = `You have clicked ${this.count} times`
+    console.log('clicked')
+  }
+}
